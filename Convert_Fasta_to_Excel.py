12be20ad05ab1c656d0eece7dddc3d4bdfdafd82
blimsanity <147113248+blimsanity@users.noreply.github.com>
2023-10-11 14:30:09 -0500

    Add files via upload

--- Convert_Fasta_to_Excel.py
+++ Convert_Fasta_to_Excel.py
@@ -1,63 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 13 17:37:53 2023
+Created on Mon Jul 17 14:36:17 2023
 
 @author: blim
 """
 
 import pandas as pd
+from Bio import SeqIO
 
-fasta_file = "/home/blim/Documents/Summer2023/HPV/HPV_aligned.fasta"
+# Specify the input and output file
+fasta_file = input("Enter the input FASTA file path: ")
+excel_file = input("Enter the output Excel file path: ")
 
-data = []  # List to store sequence data
+records = list(SeqIO.parse(fasta_file, 'fasta'))
+data = {'File': []}
+sequences = []
 
-with open(fasta_file, 'r') as file:
-    lines = file.readlines()
+# Extract sample names and sequences
+for record in records:
+    sample_name = record.id
+    sequence = str(record.seq)
+    data['File'].append(sample_name)
+    sequences.append(list(sequence))
 
-sequence = ""
-header = None
+# Prepare transposed_sequences
+max_length = max(len(seq) for seq in sequences)
+padded_sequences = [seq + ['-'] * (max_length - len(seq)) for seq in sequences]
+transposed_sequences = list(map(list, zip(*padded_sequences)))
 
-for line in lines:
-    line = line.strip()
-    if line.startswith('>'):
-        if header is not None:
-            data.append((header, sequence))
-            sequence = ""
-        header = line[1:]
-    else:
-        sequence += line
+# Add transposed sequences to the data dictionary
+for i in range(max_length):
+    column_name = f'Amino Acid {i+1}'
+    data[column_name] = transposed_sequences[i]
 
-# Append the last sequence
-data.append((header, sequence))
+# Create a DataFrame from the data dictionary
+df = pd.DataFrame(data)
 
-# Get the maximum sequence length
-max_length = max(len(seq) for _, seq in data)
+df.to_excel(excel_file, index=False)
 
-# Create a dictionary to store the sequence data for each sample
-sequence_dict = {}
-for i, (header, sequence) in enumerate(data):
-    # Pad the sequence with '-' to match the maximum length
-    padded_sequence = sequence + '-' * (max_length - len(sequence))
-    sequence_dict[f"Sample_{i+1}"] = list(padded_sequence)
-
-# Create a DataFrame from the sequence data
-df = pd.DataFrame(sequence_dict).T
-
-new_df = df.iloc[:, 27:315]
-
-# Assuming your DataFrame is named df
-new_df = new_df.replace('-', pd.NA)  # Replace '-' with missing values
-
-# Calculate the mode of each column
-column_modes = new_df.mode()
-
-# Replace missing values with the mode of each column
-new_df = new_df.fillna(column_modes.iloc[0])
-
-# If you want to replace missing values with the mode of each column, excluding the first column
-# df.iloc[:, 1:] = df.iloc[:, 1:].fillna(column_modes.iloc[0])
-
-
-# Save the DataFrame to a CSV file
-new_df.to_excel("/home/blim/Documents/Summer2023/HPV_Trial.xlsx", index=False)
+print("Data successfully processed and saved to Excel.")
